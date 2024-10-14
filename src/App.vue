727d6e0d8d46d19da45802ac71129da64156c229
Ruben Sepulveda <rsepulveda@planok.com>
2024-10-14 08:01:07 -0300

    primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TarjetaVisa/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TarjetaVisa from './components/TarjetaVisa.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TarjetaVisa
   }
 }
 </script>
